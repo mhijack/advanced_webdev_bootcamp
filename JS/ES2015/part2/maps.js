3c8cf6791b84971ccf7f77cc60df9908981a003a
mhijack <jianyuan.chen12@gmail.com>
2017-12-30 01:40:46 -0800

    add Map data structure

---
+++ JS/ES2015/part2/maps.js
@@ -0,0 +1,42 @@
+/*
+
+======= Maps ========
+1. keys can be of any data type (object keys can only be string)
+2. created with new Map;
+3. can be iterated over
+
+operations:
+map.set(key, value); // returns map
+map.delete(key); // returns true
+map.size; // returns # of keys
+map.get(1); // 'jack'
+
+map.forEach(v => console.log(v));
+
+for (val of map) {
+  // gives: [key, value]
+}
+
+    const keys = map.keys(); // returns map of keys;
+    keys.next(); // will return key in insertion order
+
+    const vlaues = map.values(); // returns map of values;
+    values.next(); // returns value in insertion order
+
+    if anything in the iterator, keys.next().done === false;
+    if iteration is over, keys.next().done === true;
+
+======= WeakMaps ========
+1. like maps, but all the keys are objects
+2. cannot be iterated over
+
+*/
+
+const map = new Map();
+map.set(1, { name: 'jack' });
+map.set(true, 'yep');
+map.set('nice', [1, 2, 3]);
+map.set([], [1, 2, 3]);
+map.set({}, { hello: 'hi' });
+map.delete(1); // true
+map.size; // 2
