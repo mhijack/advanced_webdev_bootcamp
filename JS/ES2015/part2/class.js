df7f973af205d636578dc9989401d8591c7b202c
mhijack <jianyuan.chen12@gmail.com>
2017-12-29 23:21:47 -0800

    add ES2015 class section

---
+++ JS/ES2015/part2/class.js
@@ -0,0 +1,52 @@
+/*
+
+======= 'class' keyword =======
+
+1. creates a const -> cannot be redeclared;
+2. an abstraction of constructor functions and prototypes. JavaScript does not have built in support for OOP
+3. does not hoise -> so declare class at top of file
+4. still uses 'new' keyword to create objects
+
+*/
+
+// =========== ES5 ===========
+function Student(first, last) {
+  this.first = first;
+  this.last = last;
+}
+
+// instance methods
+Student.prototype.sayHello = function() {
+  return `Hello ${this.first} ${this.last}`;
+}
+
+// class methods
+Student.isStudent = function(obj) {
+  return obj.constructor === Student;
+}
+
+const jack = new Student('jack', 'chen');
+
+// =========== ES2015 ===========
+class Student2 {
+  // constructor will be run when new is used with Student2
+  constructor(first, last) {
+    // code in constructor function is the same as in ES5
+    this.first = first;
+    this.last = last;
+  }
+
+  // instance methods - inside class with Object Shorthand Notation
+  sayHello() {
+    return `Hello ${this.first} ${this.last}`;
+  }
+
+  // static methods - equivalent to class methods in ES5
+  static isStudent(obj) {
+    // similar to Array.isArray
+    return obj.constructor === Student2;
+  }
+}
+const sum = new Student2('summer', 'han');
+// if use without new:
+// Uncaught TypeError: Class constructor Student2 cannot be invoked without 'new'
