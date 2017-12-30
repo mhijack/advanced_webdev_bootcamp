25632fd2369f3709b3a23a95cb65d201ec2a0d45
mhijack <jianyuan.chen12@gmail.com>
2017-12-30 00:14:06 -0800

    add super keyword

---
+++ JS/ES2015/part2/super.js
@@ -0,0 +1,21 @@
+class Person {
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+  }
+
+  greet() {
+    return `Hello ${first} ${last}`;
+  }
+}
+
+class Student extends Person {
+  constructor(first, last) {
+    // super must be called first before using 'this'
+    this.occupation = 'student'; // ReferenceError;
+
+    // calls the parent class' constructor with first & last provided for the Person's first and last
+    // must be of the same names!
+    super(first, last);
+  }
+}
